6e571298020b5da35e5ed7f8e3fa639166c3d81b
Hani Jazzar <hani.eljazzar@gmail.com>
2017-06-15 11:39:51 +0300

    Create batch_upload_files.py

--- batch_upload_files.py
+++ batch_upload_files.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python
 
-
 import glob
 import os
-
 
 print "Running batch upload.. Please make sure to run this script from the project directory"
 
@@ -13,34 +11,30 @@ path_to_servicekey_file = raw_input("Please enter path to 'google-servicekey.jso
 
 print "Reading files.."
 
-project_info_file = ""
-google_info_file = ""
-service_key_file = ""
+with open(path_to_project_info_file, "r") as myfile:
+    project_info_file = myfile.read()
 
-with open (path_to_project_info_file, "r") as myfile:
-	project_info_file = myfile.read()
+with open(path_to_google_info_file, "r") as myfile:
+    google_info_file = myfile.read()
 
-with open (path_to_google_info_file, "r") as myfile:
-	google_info_file = myfile.read()
-
-with open (path_to_servicekey_file, "r") as myfile:
-	service_key_file = myfile.read()
+with open(path_to_servicekey_file, "r") as myfile:
+    service_key_file = myfile.read()
 
 wrong_input = False
 
 if project_info_file == "":
-	print "Cannot read project's info.plist file"
-	wrong_input = True
+    print "Cannot read project's info.plist file"
+    wrong_input = True
 if google_info_file == "":
-	print "Cannot read google-info.plist file"
-	wrong_input = True
+    print "Cannot read google-info.plist file"
+    wrong_input = True
 if service_key_file == "":
-	print "Cannot read serviceKey-file.json"
-	wrong_input = True
+    print "Cannot read serviceKey-file.json"
+    wrong_input = True
 
-if wrong_input == True:
-	print "Error reading file(s), exiting.."
-	exit(1)
+if wrong_input:
+    print "Error reading file(s), exiting.."
+    exit(1)
 
 print "Files read successfully"
 
@@ -49,10 +43,13 @@ dysm_files = glob.glob('./*.dysm')
 print "Uploading DYSM files.."
 
 for file_name in dysm_files:
-	file_name_without_extension = file_name[2:].split('.')[0]
-	print "Uploading " + file_name_without_extension
-	os.system("../Pods/FirebaseCrash/batch-upload -i " + path_to_info + " -p " + path_to_google_info + " " + path_to_servicekey + " " + file_name_without_extension)
-	print "File" + file_name_without_extension + " Uploaded Successfully"
+    file_name_without_extension = file_name[2:].split('.')[0]
+    print "Uploading " + file_name_without_extension
+
+    os.system("../Pods/FirebaseCrash/batch-upload -i " + path_to_project_info_file + " -p " +
+              path_to_google_info_file + " " + path_to_servicekey_file + " " + file_name_without_extension)
+
+    print "File" + file_name_without_extension + " Uploaded Successfully"
 
 print "Files uploaded successfully"
 
